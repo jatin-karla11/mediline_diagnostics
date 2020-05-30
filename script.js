fe2e7b963b192e6c85e6836b4832a28dcb6fdb4f
Jatin Karla <jatinkarla8@gmail.com>
2020-05-30 12:13:12 +0530

    version 1.0.2

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 var text;
+var welcome;  
+    var date = new Date();  
+    var hour = date.getHours();  
+    var minute = date.getMinutes();  
+    var second = date.getSeconds();  
+    if (minute < 10) {  
+      minute = "0" + minute;  
+    }  
+    if (second < 10) {  
+      second = "0" + second;  
+    }  
+    if (hour < 12) {  
+      welcome = "Good Morning";  
+    } else if (hour < 17) {  
+      welcome = "Good Afternoon";  
+    } else {  
+      welcome = "Good Evening";  
+    }  
+    
+      
 $(document).ready(function() {
-  text=prompt("welcome to coding world, enter your name:","User");
-  // Fakes the loading setting a timeout
+  if(sessionStorage.getItem("name")===null)
+  {
+    text=prompt("Welcome to Mediline Diagnostics, enter your name:","User");
+    sessionStorage.setItem('name',text);
     setTimeout(function() {
-        $('body').addClass('loaded');
-        alert("hello"+ text );
-    }, 1500);   
-});
-setTimeout(function(){
+      alert(welcome +" "+text+"!");
+  }, 4000);
+  }
   
-},15000);
+  // Fakes the loading setting a timeout
+  setTimeout(function(){$('body').addClass('loaded');},2000);  
+     
+    
+});
+function exitFunction(){
+  var t=confirm("Do you really want to exit?");
+  if(t==true)
+  {
+    alert("Have a nice day!");
+  }
+  else{
+    var link=document.getElementById("exitbtn");
+    link.setAttribute("href","index.html");
+    alert("Welcome back!");
+  }
+}
